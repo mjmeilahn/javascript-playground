b2fb5bec7e9b92c98ce2496c936a84a4dffac28d
mjmeilahn <mjmeilahn@gmail.com>
2022-01-18 16:47:56 -0800

    ES6 Class example

--- 02-prototypes.js
+++ 02-prototypes.js
@@ -1,12 +1,30 @@
 
 // ES6 CLASSES ARE SYNTACTIC SUGAR FOR PROTOTYPES.
+// YOU CAN STILL EXTEND THE PROTOTYPE AS SHOWN BELOW.
+// BROWSERS, LIBRARIES & FRAMEWORKS INTERPRET
+// ES6 CLASSES AS PROTOTYPES UNDER THE HOOD.
+class Car {
+    constructor (make, model, year) {
+        this.year = year
+        this.make = make
+        this.model = model
+    }
+
+    details() {
+        return `My ${this.make} ${this.model} was made in ${this.year}.`
+    }
+}
+Car.prototype.condition = 'old'
+const car = new Car('BMW', '550i', 2020)
+// console.log(car.details())
+// console.log(car.condition)
 
 
-
+//----------------------------------------------
 // ES5: BASE OBJECT "Person" PROTOTYPE & METHODS
 
 
-
+//----------------------------------------------
 // ES5: PROTOTYPE CHAIN "Employee" FROM "Person"
 
 
